1425d1ecb80567dc0d4b7af7b6e52fac0c202d37
Paolo68 <paulmbou068@gmail.com>
2022-08-21 07:26:09 +0100

    h

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ from sqlalchemy import ARRAY, String, Integer
 class Venue(db.Model):
     __tablename__ = "venues"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
@@ -15,7 +15,7 @@ class Venue(db.Model):
     genres = db.Column(ARRAY(String))
     facebook_link = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
-    website_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(150))
     seeking_talent = db.Column(db.Boolean(), default=False)
     seeking_description = db.Column(db.String(150))
     shows = db.relationship("Show", backref="venues", lazy=True)
@@ -24,14 +24,14 @@ class Venue(db.Model):
 class Artist(db.Model):
     __tablename__ = "artists"
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String(120))
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(ARRAY(String))
     facebook_link = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
-    website_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(150))
     seeking_venue = db.Column(db.Boolean(), default=False)
     seeking_description = db.Column(db.String(150))
     shows = db.relationship("Show", backref="artists", lazy=True)
